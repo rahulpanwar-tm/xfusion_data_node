7b0097c0909e207cc6ea0af6a8cb80f00b83e143
Rahul Panwar <Rahul.Panwar@teramatrix.local>
2019-01-03 22:10:14 +0530

    pdate first

--- spider_data_node.sh
+++ spider_data_node.sh
@@ -4,7 +4,7 @@
 # To declare static Array  
 # sh spider_data_node.sh '192.168.1.122 192.168.1.56 192.168.1.122' 'Ttpl@123 Ttpl@123 Ttpl@123' 'root root root' '3307 3307 3307' '192.168.1.55';
 
-echo "SERVER_IP"  "USER_NAME"  "Password"  "Port" "Pro_Server"
+echo "ID" "SERVER_IP"  "USER_NAME"  "Password"  "Port" "Pro_Server"
 array_ip=($1); array_user=($2); array_Password=($3); array_port=($4); for ((i=0;i<=${#array_ip[@]}-1;i++)); do echo "$i" "${array_ip[i]}" "${array_user[i]}" "${array_Password[i]}" "${array_port[i]}" "$5"; done;echo ''
 #array_ip=$1; array_user=$3; array_Password=$2; array_port=$4; 
 #for ((i=0;i<=${#array_ip[@]}-1;i++))
