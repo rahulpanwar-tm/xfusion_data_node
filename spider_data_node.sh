5f187bd4ce6e0b868f673ecd5aa167d3dd6e0166
Rahul Panwar <Rahul.Panwar@teramatrix.local>
2019-01-04 13:55:09 +0530

    pdate first

--- spider_data_node.sh
+++ spider_data_node.sh
@@ -30,6 +30,7 @@ if in_array "${array_node[m]}" "${verisoning_table[*]}"
   else
     echo "Data Node" "${array_node[m]}" "not installed!!"
 # prepare sql script for current node
+pwd
 cat xfusion_data_node_2019-01-03.sql.sql >> "${array_node[m]}".sql
 # install data node and partition on particular server 
 mysql -u"${array_user[j]}" --port "${array_port[j]}"  -p"${array_Password[j]}" -c -h "$5" -e "show databases;";
