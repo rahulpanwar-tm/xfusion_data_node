295b0945214966770c791af96a6d360997875960
Rahul Panwar <Rahul.Panwar@teramatrix.local>
2019-01-04 11:45:18 +0530

    pdate first

--- spider_data_node.sh
+++ spider_data_node.sh
@@ -10,7 +10,7 @@ echo "$i" "${array_ip[i]}" "${array_user[i]}" "${array_Password[i]}" "${array_po
 done; echo ''
 if [ "$9" == "xfusion_auth_engine" ]
 then
-verisoning_table=($(mysql -u${array_user[i]} --port $PRO_PORT  -p${array_Password[i]} -c -h $5 -Bse "show databases;"))
+verisoning_table=($(mysql -u"${array_user[i]}" --port "${array_port[i]}"  -p"${array_Password[i]}" -c -h "$5" -e "show databases;"))
 echo "${verisoning_table[@]}"
 Array_exist_db=(mysql -u"${array_user[i]}" --port "${array_port[i]}"  -p"${array_Password[i]}" -c -h "$5" -Bse "show databases;")
 echo "${Array_exist_db[@]}" "Rahul Panwar"
