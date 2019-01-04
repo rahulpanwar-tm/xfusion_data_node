ad8a7db21b2948a5bf825a6a08d5c519958fdfc7
Rahul Panwar <Rahul.Panwar@teramatrix.local>
2019-01-04 12:45:07 +0530

    pdate first

--- spider_data_node.sh
+++ spider_data_node.sh
@@ -11,9 +11,9 @@ done; echo ''
 if [ "$9" == "xfusion_auth_engine" ]
 then
 #verisoning_table=($(mysql -u${array_user[i]} --port ${array_port[i]}  -p${array_Password[i]} -c -h $5 -BSe "show databases;"))
-verisoning_table='Thanks to Stephane Chazelas for the original example'
-echo "${verisoning_table[@]}"
-echo "${#verisoning_table[@]}"
+verisoning_table=('Thanks to Stephane Chazelas for the original example');
+echo "${verisoning_table[@]}";
+echo "${#verisoning_table[@]}";
 if [[ " ${verisoning_table[*]} " == *"${array_node[i]}"* ]];
 then
     echo "YES, your arr contains ${array_node[i]}"
