3aa0d84b450b54a33451171c867f49df7d7a55fc
Rahul Panwar <Rahul.Panwar@teramatrix.local>
2019-01-08 16:18:39 +0530

    updated code

--- spider_data_node.sh
+++ spider_data_node.sh
@@ -3,10 +3,10 @@
 #IFS=',' ;for itemp_ip in `echo "192.168.1.122,192.168.234.54,124.567.34.5"`;do IFS=',' ;for item_user in `echo "developer,admin,Ttpl"`;do echo $item_user $itemp_ip; done; done; echo ""
 # To declare static Array  
 # sh spider_data_node.sh '192.168.1.122 192.168.1.56 192.168.1.122' 'Ttpl@123 Ttpl@123 Ttpl@123' 'root root root' '3307 3307 3307' '192.168.1.55' 'xfusion_performance_data_node_1 xfusion_performance_data_node_2 xfusion_performance_data_node_1' 'srv_node1 srv_node2 srv_node3' 'Exicom' 'xfusion_auth_engine';
-array_ip=($1); array_user=($3); array_Password=($2); array_port=($4); array_node=($6); array_server=($7); 
+array_ip=($1); array_user=($3); array_Password=($2); array_port=($4); array_node=($6); array_server=($7); array_pro=($8);
 echo "ID" "SERVER_IP"  "USER_NAME"  "Password"  "Port" "Pro_Server" "Data_node_name" "Node_server"
 for ((i=0;i<=${#array_ip[@]}-1;i++)); do
-echo "$i" "${array_ip[i]}" "${array_user[i]}" "${array_Password[i]}" "${array_port[i]}" "$5" "${array_node[i]}" "${array_server[i]}" "$8" "$9" "$10" "$11" "$12";
+echo "$i" "${array_ip[i]}" "${array_user[i]}" "${array_Password[i]}" "${array_port[i]}" "$5" "${array_node[i]}" "${array_server[i]}" "${array_pro[0]}" "${array_pro[1]}" "${array_pro[2]}" "${array_pro[3]}" "${array_pro[4]}" ;
 done; echo ''
 if [ "$9" == "xfusion_performance_data" ]
 then
