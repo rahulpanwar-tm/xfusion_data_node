7eca4876cefbf5c318fe113a9fdaf32b14fb4f5d
Rahul Panwar <Rahul.Panwar@teramatrix.local>
2019-01-10 10:47:13 +0530

    updated

--- deployement_1.sh
+++ deployement_1.sh
@@ -158,7 +158,7 @@ then
   echo "$verisoning_table"
 
 fi
-   mysql -u$PRO_USERID --port $PRO_PORT  -p$PRO_PASSWORD -c -h $PROD_IPADDRESS -e "INSERT INTO xfusion_config.version_status (project_name,model_name, script_name, installation_date, script_date) VALUES ('$ORGANIZATION','$APPLICATION','$i',UNIX_TIMESTAMP(NOW()),unix_timestamp(substring(substring('$i' FROM -14),1,10)));"
+   mysql -u$PRO_USERID --port $PRO_PORT  -p$PRO_PASSWORD -c -h $PROD_IPADDRESS -e "INSERT INTO $versioning_db.$versioning_table (project_name,model_name, script_name, installation_date, script_date) VALUES ('$ORGANIZATION','$APPLICATION','$i',UNIX_TIMESTAMP(NOW()),unix_timestamp(substring(substring('$i' FROM -14),1,10)));"
   # mysql -u$GLOB_USERID --port $GLOB_PORT  -p$GLOB_PASSWORD -c -h $GLOB_IPADDRESS -e "use versioning; call database_deployement_version_insert('$ORGANIZATION','$APPLICATION','$i');"
    # or do whatever with individual element of the array
 done
--- spider_data_node.sh
+++ spider_data_node.sh
@@ -40,17 +40,20 @@ sed -i "s/xfusion_performance_data_node_1/${array_node[m]}/g" /home/ttpl/.jenkin
 #mysql -u"${array_user[j]}" --port "${array_port[j]}"  -p"${array_Password[j]}" -c -h "$5" -e "show databases;";
 # create server on spider node and config server
 #mysql -u"${array_user[j]}" --port "${array_port[j]}"  -p"${array_Password[j]}" -c -h "$5" -e "show databases;";
-echo "${array_pro[0]}" "${array_pro[1]}" "${array_ip[j]}" "${array_user[j]}" "${array_Password[j]}" "${array_port[j]}" "${array_pro[2]}" "${array_pro[3]}" "${array_pro[4]}"
+echo "${array_pro[0]}" "${array_pro[1]}" "${array_ip[m]}" "${array_user[m]}" "${array_Password[m]}" "${array_port[m]}" "${array_pro[2]}" "${array_pro[3]}" "${array_pro[4]}"
 echo "Rahul Panwar"
-. ./jenkins_1.sh "${array_pro[0]}" "${array_pro[1]}" "${array_ip[j]}" "${array_user[j]}" "${array_Password[j]}" "${array_port[j]}" "${array_pro[2]}" "${array_pro[3]}" "${array_pro[4]}"
-
+. ./jenkins_1.sh "${array_pro[0]}" "${array_pro[1]}" "${array_ip[m]}" "${array_user[m]}" "${array_Password[m]}" "${array_port[m]}" "${array_pro[2]}" "${array_pro[3]}" "${array_pro[4]}"
+# drop all spider tables from mysql_spider
+mysql -u$PRO_USERID --port $PRO_PORT  -p$PRO_PASSWORD -c -h $PROD_IPADDRESS -e "truncate mysql.spider_tables;" 
+# create server for particulaer node
+mysql -u$PRO_USERID --port $PRO_PORT  -p$PRO_PASSWORD -c -h $PROD_IPADDRESS -e "delete from mysql.servers where server_name='${array_server[m]}';" 
 fi
 done; echo ''
 # for ((j=0;j<=${#array_ip[@]}-1;j++)); do
 # . ./jenkins_1.sh "$8" "$9" "${array_ip[j]}" "${array_user[j]}" "${array_Password[j]}" "${array_port[j]}" "$10" "$11" "$12"
 # mysql -u"${array_user[j]}" --port "${array_port[j]}"  -p"${array_Password[j]}" -c -h "$5" -e "show databases;";
 # done; echo ''
-mysql -u$PRO_USERID --port $PRO_PORT  -p$PRO_PASSWORD -c -h $PROD_IPADDRESS -e "truncate mysql.spider_tables;" 
+
 
 elif [ "${array_pro[1]}" == "xfusion_spider_platform" ]
 then
@@ -58,9 +61,3 @@ then
 else
   echo "Both strings are different"
 fi
-#array_ip=$1; array_user=$3; array_Password=$2; array_port=$4; 
-#for ((i=0;i<=${#array_ip[@]}-1;i++))
-#do 
-#	#mysql -u${array_user[i]} --port ${array_port[i]}  -p${array_Password[i]} -c -h $5 -e "show databases;"
- 	
-# done
